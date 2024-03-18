87c8bb1cc61673e6d0981e9c3d52991f73396ed9
Daniel Kwapien <danikwapien@gmail.com>
2024-03-18 23:00:22 +0100

    add get_sentiment() and obtain_proportio() functions

--- functions.py
+++ functions.py
@@ -4,6 +4,11 @@ from transformers import pipeline
 import googleapiclient.discovery
 import googleapiclient.errors
 import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import nltk
+import lxml
+import contractions
 
 def obtain_sentiment(text):
     pipe = pipeline("text-classification", model="SamLowe/roberta-base-go_emotions")
@@ -39,3 +44,29 @@ def call_api(videoId):
 
     return pd.DataFrame(comments, columns=['author', 'published_at', 'updated_at', 'likeCount', 'text'])
 
+def wrangle_text(text):
+  # Remove HTML tags using BeautifulSoup with lxml parser
+  soup = BeautifulSoup(text, 'lxml')
+  print(soup)
+  text_without_tags = soup.get_text(separator='\n')
+
+
+  # Remove URLs using regular expression
+  url_pattern = r'https?://\S+|www\.\S+'
+  text_without_urls = re.sub(url_pattern, '', text_without_tags)
+
+  # Expand contractions
+  wrangled_text = contractions.fix(text_without_urls)
+
+  return wrangled_text
+
+
+def get_sentiment(videoId):
+    comments = call_api(videoId)
+    comments['wrangled_text'] = comments['text'].apply(wrangle_text)
+    comments['sentiment'] = comments['wrangled_text'].apply(obtain_sentiment)
+    return comments
+
+def obtain_proportion(comments):
+    sentiment_proportion = comments['sentiment'].apply(lambda x: x[0]['label']).value_counts(normalize=True)
+    return sentiment_proportion
